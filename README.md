1809316ded607ad06527643eeca5ea68b33eccff
João Pedro Patrocinio <>
2020-01-11 12:32:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MiBench Benchmark (Stripped Version for Dreams)
+
+This is a reduced set of Mibench Benchmark for DREAMS archictecture simulations.
---
+++ compile.sh
@@ -0,0 +1,30 @@
+echo "Showing GCC Version..."
+gcc -v
+
+echo "Showing GCC (RISC-V) Version..."
+riscv64-unknown-linux-gnu-gcc -v
+
+declare -a BMS_DIR=(
+	"automotive/bitcount"
+	"automotive/qsort"
+	"automotive/susan"
+	
+	"consumer/jpeg/jpeg-6a/"
+	
+	"network/dijkstra"
+	"network/patricia"
+	
+	"telecomm/CRC32"
+	"telecomm/FFT"
+	
+	"security/sha")
+
+for BM in ${BMS_DIR[@]}; do
+	echo "Compiling BM in " ${BM}
+	{
+		cd ${BM}
+		make clean
+		make
+		cd -
+	} #&> /dev/null
+done
